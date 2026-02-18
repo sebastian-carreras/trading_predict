ca93039666f19edd9f15cdcfa410ffb45ae72d98
chatoxz-wp <sebastian.carreras@automattic.com>
2026-02-18 14:53:03 -0300

    - Rrestructured the src folder to manage several models per strategy. - Created a simple but scalable Model Lifecycle Management System - Create one script to execute all e1 and another one that can compare them in a simple table.

--- src/features/intraday.py
+++
@@ -1,7 +0,0 @@
-"""Deprecated shim: use build_features_e3 instead."""
-
-from .build_features_e3 import (  # noqa: F401
-    compute_intraday_features,
-    make_sequences,
-    make_target_return,
-)
